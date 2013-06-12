fda8e8e26d87f684328620e2c8b5205f4412c953
sosandstrom <s.o.sandstrom@gmail.com>
2013-06-12 10:48:54 +0700

    ARCHETYPE registerFederated(username:basicUsername,...)

--- gaelic-archetype-starter/src/main/resources/archetype-resources/src/main/webapp/static/js/oauth2.js
+++ gaelic-archetype-starter/src/main/resources/archetype-resources/src/main/webapp/static/js/oauth2.js
@@ -5,20 +5,18 @@ function registerFederated(apiUrl, basicUsername, basicPassword, providerId) {
     var jConnection;
 
     //call to register
-    var tok = basicUsername + ':' + basicPassword;
-    var hash = window.btoa(tok);
     $.ajax({
             async: false,
             type: "GET",
             url: apiUrl + "/federated/v11",
             data:{
                     access_token: token,
-                    jBasic: hash,
                     providerId: providerId,
-//                    providerUserId: element,
                     expires_in: getParameterByName("expires_in")
             },
             dataType: "json",
+            username: basicUsername,
+            password: basicPassword,
             error: function(jqXHR, textStatus, errorThrown){
                     console.log("register fail...")
             },
