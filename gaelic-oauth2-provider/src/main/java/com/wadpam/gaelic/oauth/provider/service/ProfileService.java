108df643fd9b95ab9c32e321c006a615c0edfaf3
sosandstrom <s.o.sandstrom@gmail.com>
2013-07-24 14:30:54 +0700

    OAUTH patch secret when update profile

--- gaelic-oauth2-provider/src/main/java/com/wadpam/gaelic/oauth/provider/service/ProfileService.java
+++ gaelic-oauth2-provider/src/main/java/com/wadpam/gaelic/oauth/provider/service/ProfileService.java
@@ -40,19 +40,32 @@ public class ProfileService extends MardaoCrudService<Do2pProfile, Long, Do2pPro
         // make sure ID is generated:
         super.create(domain);
         
-        // now, encrypt the password:
-        return update(domain);
+        // now, encrypt the password (no need to patch-get):
+        return update(domain, false);
     }
     
     @Override
     public String update(Do2pProfile domain) {
+        return update(domain, true);
+    }
+    
+    public String update(Do2pProfile domain, boolean patch) {
+        if (patch) {
+            checkUsername(domain.getUsername(), domain.getId());
+            final Do2pProfile existing = dao.findByPrimaryKey(domain.getId());
+            
+            // patch properties
+            if (null != existing.getSecret() && null == domain.getSecret()) {
+                domain.setSecret(existing.getSecret());
+            }
+        }
         
-        checkUsername(domain.getUsername(), domain.getId());
-        
-        if (null != domain.getSecret() && 32 != domain.getSecret().length()) {
+        // encrypt any password not already sized 40 chars
+        if (null != domain.getSecret() && 40 != domain.getSecret().length()) {
             final String hash = ProviderService.encryptPassword(domain.getSecret(), domain.getId());
             domain.setSecret(hash);
         }
+        
         return super.update(domain);
     }
 
